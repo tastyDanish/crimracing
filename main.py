374ed3bc0374018a45c14998043e65bde5a72f13
Peter Lansdaal <peter@amplifycp.com>
2021-08-02 13:01:35 -0700

    feature updates

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ async def on_ready():
 
 # key is the player, value[0] is the emoji and value[1] is the position
 racers = {}
-lock = False
 result = []
-distance = 100
+distance = 80
 config = {
     'token': '',
     'organizer': ''
@@ -30,7 +29,7 @@ with open('token.json') as f:
 async def race_result(message):
     winners = 'THE RESULT OF THE RACE:\n'
     for i, r in enumerate(result):
-        winners += f'{i + 1}: {r}\'s racer {racers[r][0]}\n'
+        winners += f'{i + 1}: {racers[r][0]} {r}\n'
     await message.channel.send(winners)
 
 
@@ -42,18 +41,21 @@ async def race_loop(message):
     await message.channel.send('GO')
 
     while True:
-        out = f'|{"-" * distance}|\n'
+        out = f'\n╔{"═" * distance}|\n'
         round_winners = []
         for key, item in racers.items():
-            out += f"|{' ' * item[1]}{item[0]}{' ' * (distance - item[1])}"
+            # out += f"╟{'─' * item[1]}{item[0]}{'─' * (distance - item[1])}"
+            out += f"╟{'─' * item[1]}{item[0]}\n"
             if item[1] >= distance:
                 if key not in result:
                     round_winners.append(key)
-                out += '\n'
+                # out += '\n'
             else:
-                out += '|\n'
-                item[1] += random.randint(1, 10)
-        out += f'|{"-" * distance}|'
+                # out += '|\n'
+                item[1] += random.randint(1, 5)
+                if item[1] >= distance:
+                    item[1] = distance
+        out += f'╚{"═" * distance}|'
         await message.channel.send(out)
         sleep(.5)
         shuffle(round_winners)
@@ -66,7 +68,6 @@ async def race_loop(message):
 
 @client.event
 async def on_message(message):
-    global lock
     if message.author == client.user:
         return
 
@@ -74,18 +75,19 @@ async def on_message(message):
         await message.channel.send('Hello {author}!'.format(author=message.author))
 
     if message.content.startswith('!crimracing add'):
+        split = message.content.split(' ')[2]
+        if len(split) < 3:
+            await message.channel.send('I did not understand that add request')
+            return
         emoji = message.content.split(' ')[2]
-        if not emoji.startswith(':') and not emoji.endswith(':'):
-            await message.channel.send(f'{emoji} is not an emoji - try again')
         if emoji in racers.items():
             await message.channel.send(f'{emoji} is already taken. Try another one')
+            return
         racers[message.author] = [emoji, 0]
         await message.channel.send(f'I have added {racers[message.author][0]} for participant {message.author}')
 
     if message.content.startswith('!crimracing describe'):
         say = ''
-        if lock:
-            await message.channel.send(f'{message.author} the race has already begun, you cannot be included as a racer')
         if len(racers.keys()) == 0:
             await message.channel.send('There are no racers lined up')
         for key, item in racers.items():
@@ -97,11 +99,14 @@ async def on_message(message):
         result.clear()
         await message.channel.send('I have cleared the racers and the results')
 
-    if message.content.startswith('!crimracing start') and str(message.author) == config['organizer']:
-        global lock
-        lock = True
+    if message.content.startswith('!crimracing again') and str(message.author) == config['organizer']:
+        result.clear()
         await race_loop(message)
-        lock = False
+
+    if message.content.startswith('!crimracing start') and str(message.author) == config['organizer']:
+        if len(result) == len(racers.keys()):
+            await message.channel.send('Reset the race or run the again command to race again')
+        await race_loop(message)
 
     if message.content.startswith('!crimracing result'):
         await race_result(message)
